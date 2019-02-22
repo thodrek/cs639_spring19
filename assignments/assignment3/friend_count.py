c870638e60e2369c9e3fe3ecfe302190164d71c6
Theodoros Rekatsinas <thodrek@stanford.edu>
2019-02-22 10:13:46 -0600

    added assignment3

---
+++ assignments/assignment3/friend_count.py
@@ -0,0 +1,25 @@
+import MapReduce
+import sys
+
+"""
+Friend Count in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+# Implement the MAP function
+def mapper(# Provide the necessary inputs):
+    # YOUR CODE GOES HERE
+
+# Implement the REDUCE function
+def reducer(# Provide the necessary inputs):
+    # YOUR CODE GOES HERE
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
